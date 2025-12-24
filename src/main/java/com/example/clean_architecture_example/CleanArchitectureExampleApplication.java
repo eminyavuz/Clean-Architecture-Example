d70557db5acb10042ad2fa50281d5e1d8ad2ce16
Mahmut Emin Yavuz <91795112+itsaymey@users.noreply.github.com>
2025-12-24 17:16:01 +0300

    first commit

---
+++ src/main/java/com/example/clean_architecture_example/CleanArchitectureExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.clean_architecture_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchitectureExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanArchitectureExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/clean_architecture_example/CleanArchitectureExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clean_architecture_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
